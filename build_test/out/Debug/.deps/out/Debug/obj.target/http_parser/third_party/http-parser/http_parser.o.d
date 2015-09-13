eed610d72bd3e36ab760b222af2cb61e86c567a2
c9 <c9@localhost.localdomain>
2015-09-14 00:58:14 +0800

    up

--- build_test/out/Debug/.deps/out/Debug/obj.target/http_parser/third_party/http-parser/http_parser.o.d
+++
@@ -1,6 +0,0 @@
-cmd_out/Debug/obj.target/http_parser/third_party/http-parser/http_parser.o := cc '-DHTTP_PARSER_STRICT=0' '-DDEBUG' -I../third_party/http-parser  -Wall -Wno-pedantic -Wno-long-long -fvisibility=hidden -fomit-frame-pointer -fdata-sections -ffunction-sections -DGOOGLE_PROTOBUF_NO_THREAD_SAFETY -g -O0  -MMD -MF out/Debug/.deps/out/Debug/obj.target/http_parser/third_party/http-parser/http_parser.o.d.raw  -c -o out/Debug/obj.target/http_parser/third_party/http-parser/http_parser.o ../third_party/http-parser/http_parser.c
-out/Debug/obj.target/http_parser/third_party/http-parser/http_parser.o: \
- ../third_party/http-parser/http_parser.c \
- ../third_party/http-parser/http_parser.h
-../third_party/http-parser/http_parser.c:
-../third_party/http-parser/http_parser.h:
--- build_test/out/Debug/.deps/out/Debug/obj.target/libuv/third_party/libuv/src/unix/linux-syscalls.o.d
+++
@@ -1,6 +0,0 @@
-cmd_out/Debug/obj.target/libuv/third_party/libuv/src/unix/linux-syscalls.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D_GNU_SOURCE' '-DDEBUG' -I../third_party/libuv/include -I../third_party/libuv/src  -Wall -Wno-pedantic -Wno-long-long -fvisibility=hidden -fomit-frame-pointer -fdata-sections -ffunction-sections -DGOOGLE_PROTOBUF_NO_THREAD_SAFETY -pthread --std=c99 -Wno-unused-parameter -Wstrict-aliasing -g -O0  -MMD -MF out/Debug/.deps/out/Debug/obj.target/libuv/third_party/libuv/src/unix/linux-syscalls.o.d.raw  -c -o out/Debug/obj.target/libuv/third_party/libuv/src/unix/linux-syscalls.o ../third_party/libuv/src/unix/linux-syscalls.c
-out/Debug/obj.target/libuv/third_party/libuv/src/unix/linux-syscalls.o: \
- ../third_party/libuv/src/unix/linux-syscalls.c \
- ../third_party/libuv/src/unix/linux-syscalls.h
-../third_party/libuv/src/unix/linux-syscalls.c:
-../third_party/libuv/src/unix/linux-syscalls.h:
